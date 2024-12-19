47f692ae98358913fac815d281c467f42b742f68
niy42 <obanlaniyi42@gmail.com>
2024-12-19 20:12:31 +0100

    clickable footer icons

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -10,17 +10,32 @@ export default function Footer() {
 
                 </div>
                 <div className={"flex gap-3"}>
-                    <div className={"social-icon"}>
-                        <img src={"assets/github.svg"} alt={"github"} className={"w-1/2 h-1/2"}/>
-                    </div>
-                    <div className={"social-icon"}>
-                        <img src={"assets/xcom.svg"} alt={"X"} className={"w-1/2 h-1/2"}/>
-                    </div>
-                    <div className={"social-icon"}>
-                        <img src={"assets/linked.svg"} alt={"LinkedIn"} className={"w-1/2 h-1/2"}/>
-                    </div>
+                    <a
+                        href={"https://github.com/niy42"}
+                        className={"social-icon inline-block"}
+                        rel={"noreferrer noopener"}
+                    >
+                        <img src={"assets/github.svg"} alt={"Github"} className={"w-1/2 h-1/2 cursor-pointer"}/>
+                    </a>
+                    <a
+                        href={"https://x.com/@yung_0x7"}
+                        className={"social-icon inline-block"}
+                        rel={"noopener noreferrer"}
+                    >
+                        <img src={"assets/xcom.svg"} alt={"X"} className={"w-1/2 h-1/2 cursor-pointer"}/>
+                    </a>
+                    <a
+                        href={"https://www.linkedin.com/in/adeniyi-obanla-3a137914b/"}
+                        className={"social-icon inline-block"}
+                        rel={"noreferrer noopener"}
+                    >
+                        <img src={"assets/linked.svg"} alt={"LinkedIn"} className={"w-1/2 h-1/2 cursor-pointer"}/>
+                    </a>
                 </div>
-                    <p className={"text-white-600"}> <span className={"text-lg"}>©</span> 2024 Adeniyi. All rights reserved.</p>
+
+
+                <p className={"text-white-600"}><span className={"text-lg"}>©</span> 2024 Adeniyi. All rights reserved.
+                </p>
             </footer>
         </section>
     )
